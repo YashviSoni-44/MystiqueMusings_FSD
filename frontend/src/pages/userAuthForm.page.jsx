705c9383ea38c7d9fb20575cc4b423c2a6699551
YashviSoni-44 <yashvisoni599@gmail.com>
2024-08-15 23:29:26 +0530

    userauth, server, env, request rest changes

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,89 +1,4 @@
-// import React, { useContext } from 'react';
-// import { Link } from 'react-router-dom';
-// import InputBox from '../components/input.component';
-// import FaceIcon from '@mui/icons-material/Face';
-// import EmailIcon from '@mui/icons-material/AlternateEmail';
-// import LockIcon from '@mui/icons-material/Lock';
-// import googleIcon from '../imgs/google.png';
-// import back1 from '../imgs/bg1.jpg';
-// import back2 from '../imgs/bg2.jpg';
-// import AnimationWrapper from '../common/page-animation';
-// import { ThemeModeContext } from '../components/navbar.component';
-
-// const UserAuthForm = ({ type }) => {
-//     const themeMode = useContext(ThemeModeContext);
-
-//     return (
-//         <AnimationWrapper keyValue={type}>
-//             <section className="h-cover flex items-center justify-center"
-//                 style={{ 
-//                     backgroundImage: `url(${themeMode === 'dark' ? back1 : back2})`, 
-//                     backgroundSize: 'cover', 
-//                     backgroundPosition: 'center', 
-//                     height: 'auto', 
-//                     width: 'auto', 
-//                     color: themeMode === 'dark' ? 'white' : 'black'
-//                 }}
-//             >
-//                 <form className="w-[80%] max-w-[400px]">
-//                     <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-//                         {type === 'sign-in' ? 'Welcome Back' : 'Join us today'}
-//                     </h1>
-//                     {type !== 'sign-in' && (
-//                         <InputBox
-//                             name="fullname"
-//                             type="text"
-//                             placeholder="Full Name"
-//                             icon={FaceIcon}
-//                         />
-//                     )}
-//                     <InputBox
-//                         name="email"
-//                         type="email"
-//                         placeholder="Email"
-//                         icon={EmailIcon}
-//                     />
-//                     <InputBox
-//                         name="password"
-//                         type="password"
-//                         placeholder="Password"
-//                         icon={LockIcon}
-//                     />
-//                     <button className="btn-dark center mt-14" type="submit">
-//                         {type.replace('-', ' ')}
-//                     </button>
-
-//                     <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-//                         <hr className="w-1/2 border-black"></hr>
-//                         <p>OR</p>
-//                         <hr className="w-1/2 border-black"></hr>
-//                     </div>
-
-//                     <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-//                         <img src={googleIcon} className="w-5" alt="Google Icon" />
-//                         Continue with Google
-//                     </button>
-
-//                     {type === 'sign-in' ? (
-//                         <p className="mt-6 text-dark-grey text-xl text-center">
-//                             Don't have an account? 
-//                             <Link to="/signup" className="underline text-xl ml-1">Join us today</Link>
-//                         </p>
-//                     ) : (
-//                         <p className="mt-6 text-dark-grey text-xl text-center">
-//                             Already a member?
-//                             <Link to="/signin" className="underline text-xl ml-1">Sign in here</Link>
-//                         </p>
-//                     )}
-//                 </form>
-//             </section>
-//         </AnimationWrapper>
-//     );
-// };
-
-// export default UserAuthForm;
-
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import InputBox from '../components/input.component';
 import FaceIcon from '@mui/icons-material/Face';
@@ -94,9 +9,61 @@ import back1 from '../imgs/bg1.jpg';
 import back2 from '../imgs/bg2.jpg';
 import AnimationWrapper from '../common/page-animation';
 import { ThemeModeContext } from '../components/navbar.component';
+import {Toaster, toast} from 'react-hot-toast';
+import axios from 'axios';
 
 const UserAuthForm = ({ type }) => {
+
+    const userAuthThroughServer = (serverRoute,formData) =>{
+        
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+        .then(({ data }) =>{
+            console.log(data)
+        })
+        .catch(({ reponse }) => {
+            toast.error(reponse.data.error)
+        })
+    }
+
+    const authForm = useRef();
     const themeMode = useContext(ThemeModeContext);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+        
+        // Gather form data
+        let form = new FormData(authForm.current);
+        let formData = {};
+
+        for (let [key, value] of form.entries()) {
+            formData[key] = value;
+        }
+
+        let {fullname, email, password}=formData;
+
+        //form validation
+        if(fullname){
+            if(fullname.length<3){
+                return toast.error("fullname must be atleast 3 letters long")
+            }
+        }
+        if(!email.length){
+            return toast.error("enter email")
+        }
+        if(!emailRegex.test(email)){
+            return toast.error("email is invalid")
+        }
+        if(!passwordRegex.test(password)){
+            return toast.error("password should be 6-20 char. long with a num, 1 lowercase and 1 uppercase letter")
+        }
+
+        userAuthThroughServer(serverRoute,formData);
+    };
 
     return (
         <AnimationWrapper keyValue={type}>
@@ -110,7 +77,8 @@ const UserAuthForm = ({ type }) => {
                     color: themeMode === 'dark' ? 'white' : 'black'
                 }}
             >
-                <form className="w-[80%] max-w-[400px]">
+                <Toaster/>
+                <form ref={authForm} className="w-[80%] max-w-[400px]" onSubmit={handleSubmit}>
                     <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                         {type === 'sign-in' ? 'Welcome Back' : 'Join us today'}
                     </h1>
--- server/server.js
+++ server/server.js
@@ -69,6 +69,9 @@ import express from 'express'
 import mongoose from 'mongoose'
 import 'dotenv/config'
 import bcrypt from 'bcrypt'
+import {nanoid} from 'nanoid'
+import jwt from 'jsonwebtoken'
+import cors from 'cors'
 
 import User from './Schema/User.js'
 
@@ -83,6 +86,24 @@ mongoose.connect(process.env.DB_LOCATION,{
 })
 
 server.use(express.json())
+server.use(cors())
+
+const formatDatatoSend = (user) =>{
+    const access_token = jwt.sign({ id:user._id }, process.env.SECRET_ACCESS_KEY)
+    return{
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname
+    }
+}
+
+const generateUsername = async (email) =>{
+    let username=email.split("@")[0];
+    let isUsernameNotUnique = await User.exists({"personal_info.username":username}).then((result)=>result)
+    isUsernameNotUnique ? username += nanoid().substring(0, 5) : "";
+    return username
+}
 
 server.post("/signup",(req,res)=>{
     let {fullname, email, password}=req.body;
@@ -99,18 +120,49 @@ server.post("/signup",(req,res)=>{
         return res.status(403).json({"error":"password should be 6-20 char. long with a num, 1 lowercase and 1 uppercase letter"})
     }
 
-    bcrypt.hash(password, 10, (err,hashed_password)=>{
-        let username=email.split("@")[0]; 
+    bcrypt.hash(password, 10, async (err,hashed_password)=>{
+        let username=await generateUsername(email);
         let user = new User({
             personal_info:{fullname, email, password:hashed_password, username}
         })
         user.save().then((u)=>{
-            return res.status(200).json({user:u})
+            return res.status(200).json(formatDatatoSend(u))
         })
         .catch(err=>{
+            if(err.code==11000){
+                return res.status(500).json({"error":"email already exists"})
+            }
             return res.status(500).json({"error":err.message})
         })
+
         console.log(hashed_password)
+    })
+})
+
+server.post("/signin",(req,res)=>{
+    let {email, password}=req.body;
+    User.findOne({ "personal_info.email":email })
+    .then((user)=>{
+        if (!user){
+            return res.status(403).json({"error":"email not found"})
+        }
+
+        bcrypt.compare(password, user.personal_info.password, (err, result)=>{
+            if(err){
+                return res.status(403).json({"error":"Error occured while login, please try again"});
+            }
+
+            if(!result){
+                return res.status(403).json({"error":"Incorrect password"})
+            }
+            else{
+                return res.status(200).json(formatDatatoSend(user))
+            }
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        return res.status(500).json({"error":err.message})
     })
 })
 
